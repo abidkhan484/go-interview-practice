cc2802615a3f66b5ae9cf65971377907d7c4254d
Sylinsic <38617929+Sylinsic@users.noreply.github.com>
2026-08-16 13:14:05 +0100

    Add solution for Challenge 6 by Sylinsic (#1947)
    Auto-merged after 2 days with all checks passing.
    
    PR: #1947
    Author: @Sylinsic

---
+++ challenge-6/submissions/Sylinsic/solution-template.go
@@ -0,0 +1,48 @@
+// Package challenge6 contains the solution for Challenge 6.
+package challenge6
+
+import (
+	// Add any necessary imports here
+	"regexp"
+	"strings"
+)
+
+var (
+    splittingRegex = regexp.MustCompile(`[^a-z0-9]+`)
+    filteringRegex = regexp.MustCompile(`[']+`)
+)
+// CountWordFrequency takes a string containing multiple words and returns
+// a map where each key is a word and the value is the number of times that
+// word appears in the string. The comparison is case-insensitive.
+//
+// Words are defined as sequences of letters and digits.
+// All words are converted to lowercase before counting.
+// All punctuation, spaces, and other non-alphanumeric characters are ignored.
+//
+// For example:
+// Input: "The quick brown fox jumps over the lazy dog."
+// Output: map[string]int{"the": 2, "quick": 1, "brown": 1, "fox": 1, "jumps": 1, "over": 1, "lazy": 1, "dog": 1}
+func CountWordFrequency(text string) map[string]int {
+	wordCounts := make(map[string]int)
+	 
+	words := splittingRegex.Split(
+	    filteringRegex.ReplaceAllString(
+	        strings.ToLower(text), 
+            ""), 
+        -1)
+        
+	for _,word := range words {
+	    if word == "" {
+	        continue
+	    }
+	    
+	    _,exists := wordCounts[word]
+	    if !exists {
+	        wordCounts[word] = 0
+	    }
+        
+        wordCounts[word]++
+	    
+	}
+	return wordCounts
+} 
