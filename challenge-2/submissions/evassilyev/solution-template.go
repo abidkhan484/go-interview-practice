e885283bc8519983b4c86be92ea0d77bed94fbe3
Eugene Vassilyev <evgen.vasilyev@gmail.com>
2026-09-02 02:37:10 +0300

    Add solution for Challenge 3 by evassilyev (#2011)
    Auto-merged after 2 days with all checks passing.
    
    PR: #2011
    Author: @evassilyev

---
+++ challenge-2/submissions/evassilyev/solution-template.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+    runes := []rune(s)
+    for l, r:= 0, len(runes)-1; l<r; {
+        runes[l], runes[r] = runes[r], runes[l]
+        l++
+        r--
+    }
+	
+	return string(runes)
+}
