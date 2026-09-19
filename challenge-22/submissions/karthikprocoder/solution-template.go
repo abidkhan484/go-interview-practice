633f2d20152936d4215f583cac0b35a1fa579cd9
Karthik Hegde <114602991+karthikprocoder@users.noreply.github.com>
2026-09-19 22:21:07 +0530

    Add solution for Challenge 22 by karthikprocoder (#2045)
    Auto-merged after 2 days with all checks passing.
    
    PR: #2045
    Author: @karthikprocoder

---
+++ challenge-22/submissions/karthikprocoder/solution-template.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+
+*/
+
+func main() {
+	// Standard U.S. coin denominations in cents
+	denominations := []int{1, 5, 10, 25, 50}
+
+	// Test amounts
+	amounts := []int{87, 42, 99, 33, 7}
+
+	for _, amount := range amounts {
+		// Find minimum number of coins
+		minCoins := MinCoins(amount, denominations)
+
+		// Find coin combination
+		coinCombo := CoinCombination(amount, denominations)
+
+		// Print results
+		fmt.Printf("Amount: %d cents\n", amount)
+		fmt.Printf("Minimum coins needed: %d\n", minCoins)
+		fmt.Printf("Coin combination: %v\n", coinCombo)
+		fmt.Println("---------------------------")
+	}
+}
+
+// MinCoins returns the minimum number of coins needed to make the given amount.
+// If the amount cannot be made with the given denominations, return -1.
+func MinCoins(amount int, denominations []int) int {
+	minCoins := 0
+	numDenominations := len(denominations)
+	for i := numDenominations - 1; i >= 0; i-- {
+		if amount == 0 {
+			break
+		}
+		coin := denominations[i]
+		if coin <= amount {
+			numCoins := amount / coin
+			minCoins += numCoins
+			amount -= numCoins * coin
+		}
+	}
+	if amount > 0 {
+		return -1 // Cannot make the amount with given denominations
+	}
+	return minCoins
+}
+
+// CoinCombination returns a map with the specific combination of coins that gives
+// the minimum number. The keys are coin denominations and values are the number of
+// coins used for each denomination.
+// If the amount cannot be made with the given denominations, return an empty map.
+func CoinCombination(amount int, denominations []int) map[int]int {
+	coinMap := make(map[int]int)
+	numDenominations := len(denominations)
+	for i := numDenominations - 1; i >= 0; i-- {
+		if amount == 0 {
+			break
+		}
+		coin := denominations[i]
+		if coin <= amount {
+			numCoins := amount / coin
+			coinMap[coin] = numCoins
+			amount -= numCoins * coin
+		}
+	}
+	if amount > 0 {
+		return map[int]int{} // Cannot make the amount with given denominations
+	}
+	
+	return coinMap
+}
